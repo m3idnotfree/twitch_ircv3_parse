ede85d6c1f7077704a68d4f79d5050907157c630
m3id <johndoe@example.com>
2024-03-02 12:07:10 +0900

    release: 0.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,195 +1,59 @@
-use std::collections::HashMap;
+pub mod kinds;
 
-use ircv3_parse::{channel_message_str, Ircv3Parse};
+pub mod tags;
 
-mod badges;
-pub use badges::*;
-
-mod emotes;
-pub use emotes::*;
-
-mod utils;
-use serde::{Deserialize, Serialize};
-pub use utils::*;
-
-pub trait ChatFormatJson<'a> {
-    fn chat_format_json(
-        self,
-        badges_template: &BadgeTemplate,
-        emoets_template: &EmotesTemplate,
-    ) -> ChatInfo<'a>;
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct PrivMsg<'a> {
-    pub tags: Option<HashMap<&'a str, &'a str>>,
-    pub badges: Option<Vec<(&'a str, &'a str)>>,
-    pub emotes: Option<Vec<(&'a str, u64, u64)>>,
-    pub prefix: Option<(&'a str, Option<&'a str>)>,
-    pub command: &'a str,
-    pub params: HashMap<&'a str, &'a str>,
-}
-
-impl<'a> ChatFormatJson<'a> for PrivMsg<'a> {
-    fn chat_format_json(
-        self,
-        badges_template: &BadgeTemplate,
-        emoets_template: &EmotesTemplate,
-    ) -> ChatInfo<'a> {
-        let binding = self.tags.unwrap();
-
-        let time = binding.get("tmi-sent-ts").unwrap();
-        let badges = self
-            .badges
-            .map(|value| Badges::get_data(value, badges_template));
-
-        let m = self.params.get("message").unwrap();
-
-        let (_, message) = Emotes::get_data(m, self.emotes, emoets_template).unwrap();
-
-        ChatInfo::new("PRIVMSG", Some(time), badges, message)
-    }
-}
-
-impl<'a> PrivMsg<'a> {
-    pub fn new(
-        tags: Option<HashMap<&'a str, &'a str>>,
-        prefix: Option<(&'a str, Option<&'a str>)>,
-        params: &'a str,
-    ) -> PrivMsg<'a> {
-        let (_, params) = channel_message_str(params).expect("Faieled privmsg parse params");
-
-        let (tags, badges, emotes) = match tags {
-            Some(mut value) => {
-                let badges = Badges::parse(value.get("badges").unwrap());
-                let emotes = Emotes::parse(value.get("emotes").unwrap());
-
-                value.remove("badges");
-                value.remove("emotes");
-
-                (Some(value), badges, emotes)
-            }
-            None => (None, None, None),
-        };
-        PrivMsg {
-            tags,
-            badges,
-            emotes,
-            prefix,
-            command: "PRIVMSG",
-            params,
-        }
-    }
-}
+use ircv3_parse::Ircv3Parse;
+use kinds::{
+    capabilities::{
+        commands::{
+            ClearChat, ClearMsg, CommandReceive, GlobalUserState, HostTarget, Reconnect, RoomState,
+            UserNotice, UserState, Whisper,
+        },
+        Cap,
+    },
+    Member, Notice, PrivMsg,
+};
 
 #[derive(Debug, PartialEq)]
-pub enum Command {
-    Join,
-    Part,
-    Privmsg,
-    Clearchat,
-    Clearmsg,
-    Globaluserstate,
-    Hosttarget,
-    Notice,
-    Reconnect,
-    Roomstate,
-    Usernotice,
-    Userstate,
-    Whisper,
+pub enum TwitchIrcMessage<'a> {
+    Privmsg(PrivMsg<'a>),
+    Notice(Notice<'a>),
+    /// join, part
+    Member(Member),
+    Command(CommandReceive<'a>),
+    Cap(Cap<'a>),
+    Hosttarget(HostTarget),
+    Reconnect(Reconnect),
     Unknown,
+    Unimplemented,
 }
 
-#[derive(Debug, PartialEq)]
-pub enum Ircv3<'a> {
-    Priv(PrivMsg<'a>),
-    Cap(Cap),
-    Number(Number<'a>),
-    Other(Other<'a>),
-}
-#[derive(Debug, PartialEq)]
-pub struct Number<'a> {
-    command: &'a str,
-    message: &'a str,
-}
-impl<'a> Number<'a> {
-    pub fn new(command: &'a str, message: &'a str) -> Number<'a> {
-        Number { command, message }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Cap {
-    is_capabilities: bool,
-}
-
-impl Cap {
-    pub fn new(params: &str) -> Cap {
-        let (_, is_capabilities) = is_capabilities(params).unwrap();
-
-        Cap { is_capabilities }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Other<'a> {
-    pub tags: Option<HashMap<&'a str, &'a str>>,
-    pub prefix: Option<(&'a str, Option<&'a str>)>,
-    pub command: &'a str,
-    pub params: &'a str,
-}
-
-impl<'a> Other<'a> {
-    pub fn new(
-        tags: Option<HashMap<&'a str, &'a str>>,
-        prefix: Option<(&'a str, Option<&'a str>)>,
-        command: &'a str,
-        params: &'a str,
-    ) -> Other<'a> {
-        Other {
-            tags,
-            prefix,
-            command,
-            params,
-        }
-    }
-}
-
-impl<'a> ChatFormatJson<'a> for Other<'a> {
-    fn chat_format_json(
-        self,
-        _badges_template: &BadgeTemplate,
-        _emoets_template: &EmotesTemplate,
-    ) -> ChatInfo<'a> {
-        ChatInfo::new(self.command, None, None, None)
-    }
-}
-#[derive(Debug, PartialEq)]
-pub struct TwitchIrcMessage {}
-
-impl TwitchIrcMessage {
-    pub fn parse(msg: &str) -> Ircv3 {
-        let result = Ircv3Parse::new(msg);
+impl<'a> TwitchIrcMessage<'a> {
+    pub fn parse(msg: &str) -> TwitchIrcMessage {
+        let (_remain, result) = Ircv3Parse::parse(msg).unwrap();
         match result.command {
-            "CAP" => Ircv3::Cap(Cap::new(result.message)),
-            "001" => Ircv3::Number(Number::new("001", result.message)),
-            "PRIVMSG" => Ircv3::Priv(PrivMsg::new(
-                result.tags.hashmap_str(),
-                result.prefix.to_str(),
-                result.message,
+            "CAP" => TwitchIrcMessage::Cap(Cap::new(result.params)),
+            "JOIN" | "PART" => TwitchIrcMessage::Member(Member::new(
+                result.command.into(),
+                result.prefix,
+                result.params,
             )),
-            "CLEARCHAT" => Ircv3::Other(Other::new(
-                result.tags.hashmap_str(),
-                result.prefix.to_str(),
-                result.command,
-                result.message,
+            "CLEARCHAT" | "CLEARMSG" | "GLOBALUSERSTATE" | "ROOMSTATE" | "USERNOTICE"
+            | "USERSTATE" | "WHISPER" => TwitchIrcMessage::Command(CommandReceive::new(
+                result.tags,
+                result.command.into(),
+                result.prefix,
+                result.params,
             )),
-            _ => Ircv3::Other(Other::new(
-                result.tags.hashmap_str(),
-                result.prefix.to_str(),
-                result.command,
-                result.message,
-            )),
+            "NOTICE" => {
+                TwitchIrcMessage::Notice(Notice::new(result.tags, result.prefix, result.params))
+            }
+            "PRIVMSG" => {
+                TwitchIrcMessage::Privmsg(PrivMsg::new(result.tags, result.prefix, result.params))
+            }
+            "HOSTTARGET" => TwitchIrcMessage::Hosttarget(HostTarget::new(result.params)),
+            "RECONNECT" => TwitchIrcMessage::Reconnect(Reconnect::new()),
+            _ => TwitchIrcMessage::Unimplemented,
         }
     }
 }
