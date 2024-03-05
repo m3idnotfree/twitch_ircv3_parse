6647ff59c4ea442d2fbb5d82585529b07a73dfd3
m3id <johndoe@example.com>
2024-03-05 22:07:23 +0900

    feat(priv): output_json

--- src/kinds/privmsg.rs
+++ src/kinds/privmsg.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
 use ircv3_parse::{ChannelnMsg, Ircv3Params, Ircv3Prefix, Ircv3TagsParse};
+use serde::{Deserialize, Serialize};
+use twitch_highway::badges::{Badge, BadgeResponse};
 
 use crate::tags::{BadgesTag, EmotesTag};
 
@@ -37,22 +39,90 @@ impl<'a> PrivMsg<'a> {
         }
     }
 
-    pub fn get_tags(&self) -> Option<HashMap<&'a str, &'a str>> {
-        self.data.get_tags()
+    pub fn get_tags(&self) -> Option<HashMap<String, String>> {
+        self.data.tags.clone()
     }
 
-    pub fn get_badges(&self) -> Option<BadgesTag<'a>> {
+    pub fn get_badges(&self) -> Option<BadgesTag> {
         self.data.get_badges_tags()
     }
 
-    pub fn get_emotes(&self) -> Option<EmotesTag<'a>> {
+    pub fn get_emotes(&self) -> Option<EmotesTag> {
         self.data.get_emotes_tags()
     }
 
     pub fn get_prefix(&self) -> Option<(&'a str, Option<&'a str>)> {
         self.data.get_prefix()
     }
+
     pub fn get_channel_message(&self) -> ChannelnMsg {
         self.data.c_m()
     }
+
+    pub fn output_json(&self, badge: &BadgeResponse) -> OutputMessage<String> {
+        let badges = self.get_badges().map(|value| value.find_badges(badge));
+        let c_m = self.get_channel_message();
+        let emotes = self.get_emotes();
+        let emotes = emotes
+            .map(|value| value.output_message_with(&c_m.message))
+            .unwrap()
+            .unwrap();
+
+        OutputMessage {
+            kind: "message".into(),
+            channel: c_m.channel,
+            badges,
+            body: emotes,
+        }
+    }
 }
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct OutputMessage<T> {
+    #[serde(rename = "type")]
+    pub kind: String,
+    pub channel: String,
+    pub badges: Option<Vec<Badge>>,
+    pub body: Vec<MessageBoby<T>>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum MessageBoby<T> {
+    Text(Text),
+    Emote(EmoteText<T>),
+    UnknownEmote(UnknownEmote),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", rename = "text")]
+pub struct Text {
+    pub message: String,
+}
+
+impl Text {
+    pub fn new(message: String) -> Text {
+        Text { message }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", rename = "emote")]
+pub struct EmoteText<T> {
+    pub id: String,
+    pub emote: String,
+    pub data: T,
+}
+
+impl<T> EmoteText<T> {
+    pub fn new(id: String, emote: String, data: T) -> EmoteText<T> {
+        EmoteText { id, emote, data }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", rename = "unknown_emote")]
+pub struct UnknownEmote {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Output {}
