2ccd3c17a56ecb802c754c5e94c262d995628ed4
m3id <johndoe@example.com>
2024-03-05 22:04:08 +0900

    feat(ircMessageBase): method: output_json

--- src/kinds/utils.rs
+++ src/kinds/utils.rs
@@ -1,22 +1,25 @@
 use std::collections::HashMap;
 
 use ircv3_parse::{ChannelnMsg, Ircv3Params, Ircv3Prefix, Ircv3TagsParse};
+use serde::{Deserialize, Serialize};
 
 use crate::tags::{BadgesTag, EmotesTag};
 
 #[derive(Debug, PartialEq)]
 pub struct IrcMessageBase<'a> {
-    tags: Option<HashMap<&'a str, &'a str>>,
+    // tags: Option<HashMap<&'a str, &'a str>>,
+    pub tags: Option<HashMap<String, String>>,
+    // tags: Ircv3TagsParse<'a>,
     prefix: Ircv3Prefix<'a>,
     params: Ircv3Params<'a>,
 }
 
-pub trait IrcMessageTrait {
-    fn get_channel_message(&self) -> ChannelnMsg;
-    fn get_channel(&self) -> &str;
-    fn get_tags(&self) -> Option<HashMap<&str, &str>>;
-    fn get_prefix(&self) -> Option<(&str, Option<&str>)>;
-}
+// pub trait IrcMessageTrait {
+//     fn get_channel_message(&self) -> ChannelnMsg;
+//     fn get_channel(&self) -> &str;
+//     fn get_tags(&self) -> Option<HashMap<&str, &str>>;
+//     fn get_prefix(&self) -> Option<(&str, Option<&str>)>;
+// }
 
 impl<'a> IrcMessageBase<'a> {
     pub fn new(
@@ -24,26 +27,29 @@ impl<'a> IrcMessageBase<'a> {
         prefix: Ircv3Prefix<'a>,
         params: Ircv3Params<'a>,
     ) -> IrcMessageBase<'a> {
-        let tags = tags.to_hashmap_str();
+        // let ta = tags.to_hashmap_str().clone();
+        // let tag = tags.to_hashmap_string().to_owned();
 
         IrcMessageBase {
-            tags,
+            tags: tags.to_hashmap_string(),
             prefix,
             params,
         }
     }
 
-    pub fn get_badges_tags(&self) -> Option<BadgesTag<'a>> {
-        self.tags.as_ref().map(|value| {
-            let result = value.get("badges").unwrap().to_owned();
+    pub fn get_badges_tags(&self) -> Option<BadgesTag> {
+        let result = self.tags.as_ref().map(|value| {
+            let result = value.get("badges").unwrap();
 
             BadgesTag::new(result)
-        })
+        });
+        result
     }
 
-    pub fn get_emotes_tags(&self) -> Option<EmotesTag<'a>> {
+    pub fn get_emotes_tags(&self) -> Option<EmotesTag> {
+        // self.tags.clone().as_ref().map(|value| {
         self.tags.as_ref().map(|value| {
-            let result = value.get("emotes").unwrap().to_owned();
+            let result = value.get("emotes").unwrap();
             EmotesTag::new(result)
         })
     }
@@ -51,19 +57,20 @@ impl<'a> IrcMessageBase<'a> {
     /// notice
     pub fn get_msg_id_tags(&self) -> Option<String> {
         self.tags.as_ref().map(|value| {
-            let result = value.get("msg-id").unwrap().to_owned();
+            let result = value.get("msg-id").unwrap();
 
-            result.to_string()
+            result.clone()
         })
     }
     /// replay to a chat message
     pub fn get_id_tags(&self) -> Option<String> {
         self.tags.as_ref().map(|value| {
-            let result = value.get("id").unwrap().to_owned();
+            let result = value.get("id").unwrap();
 
-            result.to_string()
+            result.clone()
         })
     }
+
     pub fn c_m(&self) -> ChannelnMsg {
         let (_, channel_message) = self.params.channel_n_message().unwrap();
 
@@ -75,11 +82,34 @@ impl<'a> IrcMessageBase<'a> {
         channel
     }
 
-    pub fn get_tags(&self) -> Option<HashMap<&'a str, &'a str>> {
-        self.tags.to_owned()
-    }
+    // pub fn get_tags(&self) -> Option<HashMap<&'a str, &'a str>> {
+    //     // self.tags.to_hashmap_str().to_owned()
+    //     self.tags..to_owned()
+    // }
 
     pub fn get_prefix(&self) -> Option<(&'a str, Option<&'a str>)> {
         self.prefix.to_str()
     }
+
+    pub fn output_json(&self, command: &str) -> OutputDefaultJson {
+        // let tags = self.get_tags().into_iter().map(|(k, v)| (k, v)).collect();
+        // let tags = self.tags.to_owned().into_iter().map(|v| v.get).collect();
+        let prefix = self.prefix.to_string();
+        let command = command.to_string();
+        // let (_, params) = self.params.channel_n_message().unwrap();
+        OutputDefaultJson {
+            tags: self.tags.clone(),
+            prefix,
+            command,
+            // params,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OutputDefaultJson {
+    tags: Option<HashMap<String, String>>,
+    prefix: Option<(String, Option<String>)>,
+    command: String,
+    // params: String,
 }
