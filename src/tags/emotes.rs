395fafd5e6df93110ec2f7ae1da883a218ba76e7
m3id <johndoe@example.com>
2024-03-05 22:02:19 +0900

    feat(EmotesTag): output message

--- src/tags/emotes.rs
+++ src/tags/emotes.rs
@@ -1,5 +1,5 @@
 use nom::{
-    bytes::complete::{tag, take_until},
+    bytes::complete::{tag, take, take_until},
     character::complete::digit1,
     combinator::{map_res, opt},
     multi::{separated_list0, separated_list1},
@@ -7,18 +7,19 @@ use nom::{
     IResult,
 };
 use serde::{Deserialize, Serialize};
+
+use crate::kinds::{EmoteText, MessageBoby, Text};
 // use twitch_api::emotes::Emote;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct EmotesTag<'a> {
-    pub data: &'a str,
+pub struct EmotesTag {
+    pub data: String,
 }
 
-impl<'a> EmotesTag<'a> {
-    pub fn new(data: &'a str) -> EmotesTag {
-        EmotesTag { data }
+impl EmotesTag {
+    pub fn new<T: Into<String>>(data: T) -> EmotesTag {
+        EmotesTag { data: data.into() }
     }
-
     // emote_id, start, end, cdn
     pub fn parse(msg: &str) -> Option<Vec<(&str, u64, u64)>> {
         let (_, result) = opt(separated_list1(tag("/"), emotes_set))(msg).unwrap();
@@ -52,6 +53,94 @@ impl<'a> EmotesTag<'a> {
             }
             None => None,
         }
+    }
+
+    pub fn cdn_url(emote: &str) -> String {
+        format!(
+            "https://static-cdn.jtvnw.net/emoticons/v2/{}/default/dark/1.0",
+            emote
+        )
+    }
+
+    fn find_emote_position(msg: &str, s: u64, e: u64) -> IResult<&str, (&str, &str)> {
+        tuple((take(s), take(e)))(msg)
+    }
+
+    pub fn output_message_with(&self, msg: &str) -> Option<Vec<MessageBoby<String>>> {
+        let result = EmotesTag::parse(self.data.as_str());
+
+        result.map(|value| {
+            let mut result = vec![];
+            let mut cur = 0;
+            let mut remain = msg;
+
+            for (emote, start_emote, end_emote) in value.into_iter() {
+                let start = start_emote - cur;
+                let expeced_location = end_emote - start_emote + 1;
+
+                let (remain2, (prev, expected)) =
+                    EmotesTag::find_emote_position(remain, start, expeced_location).unwrap();
+
+                remain = remain2;
+                cur = end_emote + 1;
+
+                if !(prev.is_empty()
+                    || prev.len() == 1 && prev.chars().next().unwrap().is_whitespace())
+                {
+                    result.push(MessageBoby::Text(Text::new(prev.into())));
+                };
+
+                result.push(MessageBoby::Emote(EmoteText::new(
+                    emote.into(),
+                    expected.to_string(),
+                    EmotesTag::cdn_url(emote),
+                )))
+            }
+
+            if !remain.is_empty() {
+                result.push(MessageBoby::Text(Text::new(remain.into())));
+            }
+            result
+        })
+        // match result {
+        //     // None => Ok(("", None)),
+        //     None => None,
+        //     Some(value) => {
+        //         let mut result = vec![];
+        //         let mut cur = 0;
+        //         let mut remain = msg;
+        //
+        //         for (emote, start_emote, end_emote) in value.into_iter() {
+        //             let start = start_emote - cur;
+        //             let expeced_location = end_emote - start_emote + 1;
+        //
+        //             // let (remain2, (prev, expected)) =
+        //             //     tuple((take(start), take(expeced_location)))(remain)?;
+        //             let (remain2, (prev, expected)) =
+        //                 EmotesTag::find_emote_position(remain, start, expeced_location).unwrap();
+        //
+        //             remain = remain2;
+        //             cur = end_emote + 1;
+        //
+        //             if !(prev.is_empty()
+        //                 || prev.len() == 1 && prev.chars().next().unwrap().is_whitespace())
+        //             {
+        //                 result.push(MessageBoby::Text(Text::new(prev.into())));
+        //             };
+        //
+        //             result.push(MessageBoby::Emote(EmoteText::new(
+        //                 emote.into(),
+        //                 expected.to_string(),
+        //                 EmotesTag::cdn_url(emote),
+        //             )))
+        //         }
+        //
+        //         if !remain.is_empty() {
+        //             result.push(MessageBoby::Text(Text::new(remain.into())));
+        //         }
+        //         Some(result)
+        //     }
+        // }
     }
 
     // pub fn parse_message_with_emotes_cdn(
